e851f9794a4f828a8bac09b29ba0d36d42c8754a
mykytam <morar.nikita@gmail.com>
2020-06-03 18:08:22 +0300

    Created JPA repository. Updated pom file.

--- src/main/java/com/mykytam/courseapi/topic/Topic.java
+++ src/main/java/com/mykytam/courseapi/topic/Topic.java
@@ -4,12 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Topic {
 
+    @Id
     private String id;
+
     private String name;
     private String description;
 
---
+++ src/main/java/com/mykytam/courseapi/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.mykytam.courseapi.topic;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicRepository extends JpaRepository<Topic, String> {
+
+}
