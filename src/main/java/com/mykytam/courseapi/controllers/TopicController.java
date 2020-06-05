a71298e46695f4bc10b13be213dec2d0b65ed1a6
mykytam <morar.nikita@gmail.com>
2020-06-05 12:34:33 +0300

    Controller annotations changed

--- src/main/java/com/mykytam/courseapi/controllers/TopicController.java
+++ src/main/java/com/mykytam/courseapi/controllers/TopicController.java
@@ -2,11 +2,13 @@ package com.mykytam.courseapi.controllers;
 
 import com.mykytam.courseapi.models.Topic;
 import com.mykytam.courseapi.services.TopicService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("topics")
 public class TopicController {
 
     private final TopicService topicService;
@@ -15,27 +17,29 @@ public class TopicController {
         this.topicService = topicService;
     }
 
-    @RequestMapping("/topics")
+    @GetMapping
     public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
     }
 
-    @RequestMapping("/topics/{id}")
+    @GetMapping("{id}")
     public Topic getTopic(@PathVariable String id) {
         return topicService.getTopic(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void addTopic(@RequestBody Topic topic) {
         topicService.addTopic(topic);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    @PutMapping("{id}")
     public void updateTopic(@RequestBody Topic topic) {
         topicService.updateTopic(topic);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTopic(@PathVariable String id) {
         topicService.deleteTopic(id);
     }
