e09a8fa7c944563778fa7071f35bee3ae952e0da
mykytam <morar.nikita@gmail.com>
2020-06-10 12:54:22 +0300

    Add DTOs for Student and Topic creation

---
+++ src/main/java/com/mykytam/courseapi/dto/TopicCreateDto.java
@@ -0,0 +1,25 @@
+package com.mykytam.courseapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TopicCreateDto {
+
+    @NotNull
+    @Min(1)
+    private Integer id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+}
